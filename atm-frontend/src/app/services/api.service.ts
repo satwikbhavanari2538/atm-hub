84f0ec9b27fcfa1ab1ca83e1b58c5b2fc39ff2ec
Satwik <satwikbhavanari5@gmail.com>
2026-02-17 16:52:52 +0000

    Fix: Restored ApiService and synchronized King Claw build

---
+++ atm-frontend/src/app/services/api.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private baseUrl = 'http://64.227.187.126:8001';
+
+  constructor(private http: HttpClient) { }
+
+  getTasks(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/tasks`);
+  }
+
+  createTask(task: any): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}/tasks`, task);
+  }
+
+  updateTask(taskId: string, updates: any): Observable<any> {
+      return this.http.patch<any>(`${this.baseUrl}/tasks/${taskId}`, updates);
+  }
+
+  getAgents(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/agents`);
+  }
+
+  getStatus(): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/`);
+  }
+}
